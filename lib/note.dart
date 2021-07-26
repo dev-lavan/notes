5189f2f50d14ee10426d2ef3ea1bf72ad93ebb3d
lavan <kevinmartain@gmil.com>
2021-07-26 08:58:20 +0530

    enter

--- lib/note.dart
+++ lib/note.dart
@@ -1,4 +1,5 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'dart:async';
@@ -17,56 +18,62 @@ class _NoteDisplayState extends State<NoteDisplay> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      floatingActionButton: FloatingActionButton(
-          child: Icon(Icons.add),
-          onPressed: () {
-            Navigator.push(
-                context, MaterialPageRoute(builder: (context) => AddNote()));
-          }),
-      appBar: AppBar(title: Text("Notes")),
-      body: Container(
-        color: Colors.black,
-        child: StreamBuilder<QuerySnapshot<Map<String, dynamic>>>(
-            stream: FirebaseFirestore.instance.collection('notes').orderBy("Data", descending: true).snapshots(),
-            builder: (context,
-                AsyncSnapshot<QuerySnapshot<Map<String, dynamic>>> snapshot) {
-              if (!snapshot.hasData) {
-                return Center(child: CircularProgressIndicator());
-              }
-
-              return ListView.builder(
-                  itemCount: snapshot.data!.docs.length,
-                  itemBuilder: (context, index) {
-                    bool tick = false;
-                    TextDecoration? notechecked = null;
-
-                    return StatefulBuilder(
-                      builder: (BuildContext context,StateSetter setState) {
-                        return CheckboxListTile(
-                          //tileColor: Colors.white,
-                          value: tick,
-                          onChanged: (value) {
-                            setState(() {
-                              tick = value!;
-                              if(!tick)
-                                notechecked = null;
-                              else if(tick)
-                                notechecked = TextDecoration.lineThrough;
-                            });
-                          },
-                          title: Text(
-                            snapshot.data!.docs[index]['Data'],
-                            style: TextStyle(
-                              color:Colors.white,
-                              decoration: notechecked
-                            ),
-                            ),
-                        );
-                      },
-                    );
-                  });
+    return WillPopScope(
+      onWillPop: () async => false,
+          child: Scaffold(
+        floatingActionButton: FloatingActionButton(
+            child: Icon(Icons.add),
+            onPressed: () {
+              Navigator.push(
+                  context, MaterialPageRoute(builder: (context) => AddNote()));
             }),
+        appBar: AppBar(
+          actions: [IconButton(onPressed: (){ FirebaseAuth.instance.signOut(); Navigator.pop(context);}, icon: Icon(Icons.logout, color: Colors.red,))],
+          title: Text("Notes")
+          ),
+        body: Container(
+          color: Colors.black,
+          child: StreamBuilder<QuerySnapshot<Map<String, dynamic>>>(
+              stream: FirebaseFirestore.instance.collection('notes').orderBy("Data", descending: true).snapshots(),
+              builder: (context,
+                  AsyncSnapshot<QuerySnapshot<Map<String, dynamic>>> snapshot) {
+                if (!snapshot.hasData) {
+                  return Center(child: CircularProgressIndicator());
+                }
+
+                return ListView.builder(
+                    itemCount: snapshot.data!.docs.length,
+                    itemBuilder: (context, index) {
+                      bool tick = false;
+                      TextDecoration? notechecked = null;
+
+                      return StatefulBuilder(
+                        builder: (BuildContext context,StateSetter setState) {
+                          return CheckboxListTile(
+                            //tileColor: Colors.white,
+                            value: tick,
+                            onChanged: (value) {
+                              setState(() {
+                                tick = value!;
+                                if(!tick)
+                                  notechecked = null;
+                                else if(tick)
+                                  notechecked = TextDecoration.lineThrough;
+                              });
+                            },
+                            title: Text(
+                              snapshot.data!.docs[index]['Data'],
+                              style: TextStyle(
+                                color:Colors.white,
+                                decoration: notechecked
+                              ),
+                              ),
+                          );
+                        },
+                      );
+                    });
+              }),
+        ),
       ),
     );
   }
